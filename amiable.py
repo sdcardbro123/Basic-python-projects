22d5c013825ccb2966a3a631ea7923fb3d83e21c
Suprateek Dhar <118747426+sdcardbro123@users.noreply.github.com>
2023-10-14 09:39:20 +0530

    Update amiable.py
    doing it

--- amiable.py
+++ amiable.py
@@ -1,1 +1,19 @@
+x = int(input("Enter no. "))
+y = int(input("Enter no. "))
 
+xd = []
+
+for i in range(1, x+1):
+    if (x%i ==0):
+        xd.append(i)
+print(xd)
+
+z = int(1)
+for j in range(1,len(xd)):
+    z += xd[j]
+print(z)
+
+if x+y==z:
+    print("Amiable")
+else:
+    print("NOPE")
